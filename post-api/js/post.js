486e79f9fbe6f7922c07a7bee3c71f398f0cc818
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-08-28 22:22:29 +0600

    using id and display details

--- post-api/js/post.js
+++ post-api/js/post.js
@@ -1,13 +1,18 @@
 function loadPosts(){
+    try{
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
     .then(data => getPosts(data))
+    }
+    catch(error){
+        console.log(error)
+    }
+    
 }
 
 function getPosts(data){
     const postConatainer = document.getElementById('post-container');
     for(const post of data){
-        console.log(post)
         const div = document.createElement('div');
         div.classList = 'post';
         const title = post.title;
@@ -16,9 +21,32 @@ function getPosts(data){
         <h4>${post.id}</h4>
         <h2>${title}</h2>
         <p>${body}</p>
+        <a href="#" onclick="loadPostId(${post.id})" class="btn btn-primary">More Info</a>
         `
         postConatainer.appendChild(div);
 
     }
 }
 loadPosts();
+
+const loadPostId = (postId) =>{
+    const url = `https://jsonplaceholder.typicode.com/posts/${postId}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => loadPostDetails(data))
+
+}
+
+const loadPostDetails = (data) =>{
+    console.log(data);
+    const postDetailContainer = document.getElementById('load-post-detail');
+    postDetailContainer.textContent = ' ';
+    const detailDiv = document.createElement('div');
+    detailDiv.classList = 'detail-info';
+    detailDiv.innerHTML = `
+        <h4>${data.id}</h4>
+        <h2>${data.title}</h2>
+        <p>${data.body}</p>
+    `
+    postDetailContainer.appendChild(detailDiv);
+}
